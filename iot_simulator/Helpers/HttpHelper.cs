c0798d8994d0e5998eb4d842e24d978f135c7768
mgSantiago <matheussantiago@gmail.com>
2019-10-27 00:33:29 -0300

    Commit inicial

---
+++ iot_simulator/Helpers/HttpHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iot_simulator.Helpers
+{
+    public class HttpHelper
+    {
+
+    }
+}
---
+++ iot_simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iot_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
